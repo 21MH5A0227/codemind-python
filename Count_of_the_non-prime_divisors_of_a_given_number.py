fd910e78d72b5b87cc9889b5f4936c9432476f2d
21MH5A0227 <102216646+21MH5A0227@users.noreply.github.com>
2022-09-24 22:54:36 +0530

    n=int(input()) l=[] l1=[] for i in range(1,n+1):     if n%i==0:         l.append(i) for i in range(len(l)):     c=0     for j in range(1,l[i]+1):         if(l[i]%j==0):             c+=1     if c==2:         pass     else:         l1.append(l[i]) print(len(l1))

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,17 +1,16 @@
-import math
-def fun(x):
-    if x==1:
-        return False
-    for i in range(2,int(math.sqrt(x))+1):
-        if x%i==0:
-            return False
-    return True
 n=int(input())
 l=[]
+l1=[]
 for i in range(1,n+1):
-    if fun(i):
-        continue
+    if n%i==0:
+        l.append(i)
+for i in range(len(l)):
+    c=0
+    for j in range(1,l[i]+1):
+        if(l[i]%j==0):
+            c+=1
+    if c==2:
+        pass
     else:
-        if n%i==0:
-            l.append(i)
-print(len(l))
+        l1.append(l[i])
+print(len(l1))
