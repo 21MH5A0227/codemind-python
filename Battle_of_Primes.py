b2544e9f9e7bd3f4cc145225a472e4000a0696f2
21MH5A0227 <102216646+21MH5A0227@users.noreply.github.com>
2022-09-24 23:11:27 +0530

    n1=int(input()) n2=int(input()) if n1>n2:     n1,n2=n2,n1 n=n1+n2+100 t=n1+n2 l=[] for i in range(t+1,n):     c=0     for j in range(1,i+1):         if(i%j==0):             c+=1     if c==2:         l.append(i) print(l[0]-t)

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,17 +1,15 @@
-import math
-def fun(x):
-    if x==1:
-        return False
-    for i in range(2,int(math.sqrt(x))+1):
-        if x%i==0:
-            return False
-    return True
-n=int(input())
-m=int(input())
-c=n+m
-t=c
-while True:
-    c=c+1
-    if fun(c):
-        break
-print(c-t)
+n1=int(input())
+n2=int(input())
+if n1>n2:
+    n1,n2=n2,n1
+n=n1+n2+100
+t=n1+n2
+l=[]
+for i in range(t+1,n):
+    c=0
+    for j in range(1,i+1):
+        if(i%j==0):
+            c+=1
+    if c==2:
+        l.append(i)
+print(l[0]-t)
