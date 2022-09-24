bddec373ce791d8d4de7cd38c3786765ece9bf5f
21MH5A0227 <102216646+21MH5A0227@users.noreply.github.com>
2022-09-24 23:27:43 +0530

    num=int(input()) k=0 for j in range(2,num):     if num%j==0:         k+=1 if k==0:     c=0     s=0     while num!=0:         r=num%10         s=s*10+r         num=num//10     for i in range(1,s+1):         if s%i==0:             c+=1     if c==2:         print("circular prime")     else:         print("prime but not a circular prime") else:     print("not prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,15 +1,21 @@
-import math
-def fun(n):
-    for i in range(2,int(math.sqrt(n))+1):
-        if n%i==0:
-            return False
-    return True
-n=int(input())
-n1=str(n)
-n1=int(n1[::-1])
-if fun(n) and fun(n1):
-    print("circular prime")
-elif fun(n) or fun(n1):
-    print("prime but not a circular prime")
+num=int(input())
+k=0
+for j in range(2,num):
+    if num%j==0:
+        k+=1
+if k==0:
+    c=0
+    s=0
+    while num!=0:
+        r=num%10
+        s=s*10+r
+        num=num//10
+    for i in range(1,s+1):
+        if s%i==0:
+            c+=1
+    if c==2:
+        print("circular prime")
+    else:
+        print("prime but not a circular prime")
 else:
     print("not prime")
