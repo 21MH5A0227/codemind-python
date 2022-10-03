d6a848500bbf9883ec8796767c8796a94b64851a
21MH5A0227 <102216646+21MH5A0227@users.noreply.github.com>
2022-10-03 07:21:46 +0530

    num=int(input()) l=list(map(int,input().split())) n=int(input()) l1=[] c=0 c1=0 for i in range(len(l)):     if(n<=l[i]):         l1.append(l[i]) for j in range(len(l1)):     c=0     for k in range(1,l1[j]):         if(l1[j]%k==0):             c+=1     if(c==1):         c1=c1+1 print(c1)

---
+++ Primes_greater_than_k.py
@@ -0,0 +1,17 @@
+num=int(input())
+l=list(map(int,input().split()))
+n=int(input())
+l1=[]
+c=0
+c1=0
+for i in range(len(l)):
+    if(n<=l[i]):
+        l1.append(l[i])
+for j in range(len(l1)):
+    c=0
+    for k in range(1,l1[j]):
+        if(l1[j]%k==0):
+            c+=1
+    if(c==1):
+        c1=c1+1
+print(c1)
