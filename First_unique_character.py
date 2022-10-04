81380541128ecb1b830a69d557100e721f7ac44a
21MH5A0227 <102216646+21MH5A0227@users.noreply.github.com>
2022-10-04 11:54:13 +0530

    n=input() n=list(n) l=[] c=0 for i in range(len(n)):     if(n.count(n[i])==1):         if(n[i])!=" ":             l.append(n[i])             c+=1 if(c==0):     print(-1) print(l[0])

---
+++ First_unique_character.py
@@ -0,0 +1,12 @@
+n=input()
+n=list(n)
+l=[]
+c=0
+for i in range(len(n)):
+    if(n.count(n[i])==1):
+        if(n[i])!=" ":
+            l.append(n[i])
+            c+=1
+if(c==0):
+    print(-1)
+print(l[0])
